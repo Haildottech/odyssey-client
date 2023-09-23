2a2375cc32b6d58affc7d13b0b390de868b3d8f0
Haildottech <141232199+Haildottech@users.noreply.github.com>
2023-09-23 11:09:19 +0000

    update

--- Components/Layouts/Dashboard/Requests/index.js
+++ Components/Layouts/Dashboard/Requests/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap';
-import readXlsxFile from 'read-excel-file'
+//import readXlsxFile from 'read-excel-file'
 
 const Requests = ({sessionData}) => {
 
@@ -14,13 +14,7 @@ const Requests = ({sessionData}) => {
 
   return (
     <div className='base-page-layout'>
-        <input type="file" value={files} onChange={(e)=>{
-          setFiles(e.target.value)
-            readXlsxFile(e.target.files[0]).then((rows) => {
-              console.log(rows.splice(10, 1594))
-            })
-
-        }} />
+        
     </div>
   )
 }
