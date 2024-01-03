e2314976f7b7311edc27f9da2dcc206d62a7b7d9
Haildottech <haildottech@gmail.com>
2024-01-03 14:31:45 +0500

    added pagination + searchbar on jobs

--- Components/Layouts/JobsLayout/JobsList.js
+++ Components/Layouts/JobsLayout/JobsList.js
@@ -5,134 +5,164 @@ import React, { useEffect, useState } from 'react';
 import { Row, Col, Table } from 'react-bootstrap';
 import Router from 'next/router';
 import { useQueryClient } from '@tanstack/react-query';
+import Form from 'react-bootstrap/Form';
+import Pagination from '../../Shared/Pagination';
 
 const SEJobList = ({ jobsData, sessionData, type }) => {
-
   const queryClient = useQueryClient();
-  const changedValues = useSelector((state)=>state.persistValues);
+  const changedValues = useSelector((state) => state.persistValues);
   const companyId = useSelector((state) => state.company.value);
   const [records, setRecords] = useState([]);
+  const [query, setQuery] = useState("");
   const dispatch = useDispatch();
+  //search state
+  const keys = ["jobNo"]
+  //pagination states
+  const [currentPage,setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(5);
+
+  const indexOfLast = currentPage * recordsPerPage;
+  const indexOfFirst = indexOfLast - recordsPerPage;
+  const currentRecords = records.slice(indexOfFirst,indexOfLast);
+  const noOfPages = Math.ceil(records.length / recordsPerPage);
 
   useEffect(() => {
-    if(jobsData.status=="success"){
+    if (jobsData.status == "success") {
       setRecords(jobsData.result);
     }
   }, [])
 
-  return(
-  <>
-    {companyId!='' &&
-    <div className='base-page-layout'>
-      <Row>
-      <Col md={10}>
-        <h5>
-          {type=="SE"?"SEA Export":type=="SI"?"SEA Import":type=="AE"?"AIR Export":type=="AI"?"AIR Import":""} Job List
-        </h5>
-        </Col>
-      <Col md={1}>
-        <button className='btn-custom right'
-          onClick={()=>{
-            Router.push(`/seaJobs/jobList`)
-          }}
-        >List</button>
-      </Col>
-      <Col md={1}>
-        <button className='btn-custom right'
-          onClick={()=>{
-            queryClient.removeQueries({ queryKey: ['jobData',{ type }] })
-            let obj = {...changedValues.value}
-            obj[type] = ""
-            dispatch(addValues(obj));
-            dispatch(incrementTab({
-              "label":type=="SE"?"SE JOB":type=="SI"?"SI JOB":type=="AE"?"AE JOB":"AI JOB",
-              "key":type=="SE"?"4-3":type=="SI"?"4-6":type=="AE"?"7-2":"7-5",
-              "id":"new"
-            }));
-            Router.push(
-              type=="SE"?`/seaJobs/export/new`:
-              type=="SI"?`/seaJobs/import/new`:
-              type=="AE"?`/airJobs/export/new`:
-              `/airJobs/import/new`
-            )
-          }}
-        >Create</button>
-      </Col>
-      </Row>
-      <hr className='my-2' />
-      <div className='mt-3' style={{maxHeight:500, overflowY:'auto'}}>
-      <Table className='tableFixHead'>
-        <thead>
-          <tr>
-            <th>Sr.</th>
-            <th>Basic Info</th>
-            <th>Shipment Info</th>
-            <th>Weight Info</th>
-            <th>Other Info</th>
-            <th>Status</th>
-            <th>Created By</th>
-          </tr>
-        </thead>
-        <tbody>
-        {
-        records.map((x, index) => {
-        return (
-          <tr key={index} className='f row-hov'
-            onClick={() => {
-              queryClient.removeQueries({ queryKey: ['jobData',{ type }] })
-              let obj = {...changedValues.value}
-              obj[type] = ""
-              dispatch(addValues(obj));
-              dispatch(incrementTab({
-                "label":type=="SE"?"SE JOB":type=="SI"?"SI JOB":type=="AE"?"AE JOB":"AI JOB",
-                "key":type=="SE"?"4-3":type=="SI"?"4-6":type=="AE"?"7-2":"7-5",
-                "id":x.id
-              }))
-              Router.push(
-                type=="SE"?`/seaJobs/export/${x.id}`:
-                type=="SI"?`/seaJobs/import/${x.id}`:
-                type=="AE"?`/airJobs/export/${x.id}`:
-                `/airJobs/import/${x.id}`
-              )
-            }}
-          >
-            <td>{index + 1}</td>
-            <td>
-              <span className='blue-txt fw-7'>{x.jobNo}</span>
-              <br/>Nomination: <span className='grey-txt'>{x.nomination}</span>
-              <br/>Freight Type: <span className='grey-txt'>{x.freightType}</span>
-              <br/>Sub Type: <span className='grey-txt '>{x.subType}</span><br/>
-            </td>
-            <td>
-              POL: <span className='grey-txt'>{x.pol}</span><br/>
-              POD: <span className='grey-txt'>{x.pod}</span><br/>
-              FLD: <span className='grey-txt'> {x.fd}</span>
-            </td>
-            <td>
-              {/* Container: <span className='grey-txt'>{x.container}</span><br/> */}
-              Weight: <span className='grey-txt'>{x.weight}</span>
-            </td>
-            <td>
-              Party:<span className='blue-txt fw-5'> {x.Client===null?"":x.Client.name}</span><br/>
-              Transportion: <span className='blue-txt fw-5'>{x.transportCheck!=''?'Yes':'No'}</span>
-              <br/>
-              Custom Clearance: <span className='blue-txt fw-5'>{x.customCheck!=''?'Yes':'No'}</span>
-            </td>
-            <td>
-              {x.approved=="true"?<img src={'/approve.png'} height={70} className='' />:"Not Approved"}
-            </td>
-            <td className='blue-txt fw-6'>
-              {x.created_by?.name}
-            </td>
-          </tr>
-          )
-        })}
-        </tbody>
-      </Table>
-      </div>
-    </div>
-    }
-  </>
-)}
+  const search = (data) => {
+    return data.filter(item => {
+      return keys.some(key => item[key].toLowerCase().includes(query.toLowerCase()));
+    });
+  };
+
+  useEffect(() => {
+    const filteredData = search(jobsData.result);
+    setRecords(filteredData)
+  }, [jobsData, query])
+
+  return (
+    <>
+      {companyId != '' &&
+        <div className='base-page-layout'>
+          <Row>
+            <Col md={8}>
+              <h5>
+                {type == "SE" ? "SEA Export" : type == "SI" ? "SEA Import" : type == "AE" ? "AIR Export" : type == "AI" ? "AIR Import" : ""} Job List
+              </h5>
+            </Col>
+            <Col md="2">
+              <Form.Control type="text" placeholder="Search..." size='sm' onChange={e => setQuery(e.target.value)} />
+            </Col>
+            <Col md={1}>
+              <button className='btn-custom right'
+                onClick={() => {
+                  Router.push(`/seaJobs/jobList`)
+                }}
+              >List</button>
+            </Col>
+            <Col md={1}>
+              <button className='btn-custom right'
+                onClick={() => {
+                  queryClient.removeQueries({ queryKey: ['jobData', { type }] })
+                  let obj = { ...changedValues.value }
+                  obj[type] = ""
+                  dispatch(addValues(obj));
+                  dispatch(incrementTab({
+                    "label": type == "SE" ? "SE JOB" : type == "SI" ? "SI JOB" : type == "AE" ? "AE JOB" : "AI JOB",
+                    "key": type == "SE" ? "4-3" : type == "SI" ? "4-6" : type == "AE" ? "7-2" : "7-5",
+                    "id": "new"
+                  }));
+                  Router.push(
+                    type == "SE" ? `/seaJobs/export/new` :
+                      type == "SI" ? `/seaJobs/import/new` :
+                        type == "AE" ? `/airJobs/export/new` :
+                          `/airJobs/import/new`
+                  )
+                }}
+              >Create</button>
+            </Col>
+          </Row>
+          <hr className='my-2' />
+          <div className='mt-3' style={{ maxHeight: 600, overflowY: 'auto' }}>
+            <Table className='tableFixHead'>
+              <thead>
+                <tr>
+                  <th>Sr.</th>
+                  <th>Basic Info</th>
+                  <th>Shipment Info</th>
+                  <th>Weight Info</th>
+                  <th>Other Info</th>
+                  <th>Status</th>
+                  <th>Created By</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  currentRecords.map((x, index) => {
+                    return (
+                      <tr key={index} className='f row-hov'
+                        onClick={() => {
+                          queryClient.removeQueries({ queryKey: ['jobData', { type }] })
+                          let obj = { ...changedValues.value }
+                          obj[type] = ""
+                          dispatch(addValues(obj));
+                          dispatch(incrementTab({
+                            "label": type == "SE" ? "SE JOB" : type == "SI" ? "SI JOB" : type == "AE" ? "AE JOB" : "AI JOB",
+                            "key": type == "SE" ? "4-3" : type == "SI" ? "4-6" : type == "AE" ? "7-2" : "7-5",
+                            "id": x.id
+                          }))
+                          Router.push(
+                            type == "SE" ? `/seaJobs/export/${x.id}` :
+                              type == "SI" ? `/seaJobs/import/${x.id}` :
+                                type == "AE" ? `/airJobs/export/${x.id}` :
+                                  `/airJobs/import/${x.id}`
+                          )
+                        }}
+                      >
+                        <td>{index + 1}</td>
+                        <td>
+                          <span className='blue-txt fw-7'>{x.jobNo}</span>
+                          <br />Nomination: <span className='grey-txt'>{x.nomination}</span>
+                          <br />Freight Type: <span className='grey-txt'>{x.freightType}</span>
+                          <br />Sub Type: <span className='grey-txt '>{x.subType}</span><br />
+                        </td>
+                        <td>
+                          POL: <span className='grey-txt'>{x.pol}</span><br />
+                          POD: <span className='grey-txt'>{x.pod}</span><br />
+                          FLD: <span className='grey-txt'> {x.fd}</span>
+                        </td>
+                        <td>
+                          {/* Container: <span className='grey-txt'>{x.container}</span><br/> */}
+                          Weight: <span className='grey-txt'>{x.weight}</span>
+                        </td>
+                        <td>
+                          Party:<span className='blue-txt fw-5'> {x.Client === null ? "" : x.Client.name}</span><br />
+                          Transportion: <span className='blue-txt fw-5'>{x.transportCheck != '' ? 'Yes' : 'No'}</span>
+                          <br />
+                          Custom Clearance: <span className='blue-txt fw-5'>{x.customCheck != '' ? 'Yes' : 'No'}</span>
+                        </td>
+                        <td>
+                          {x.approved == "true" ? <img src={'/approve.png'} height={70} className='' /> : "Not Approved"}
+                        </td>
+                        <td className='blue-txt fw-6'>
+                          {x.created_by?.name}
+                        </td>
+                      </tr>
+                    )
+                  })}
+              </tbody>
+            </Table>
+          </div>
+          <div className=''style={{maxWidth:"50%",position:"fixed", bottom:20, right:25}} >
+                <Pagination noOfPages={noOfPages} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+          </div>
+        </div>
+      }
+    </>
+  )
+}
 
 export default SEJobList;
