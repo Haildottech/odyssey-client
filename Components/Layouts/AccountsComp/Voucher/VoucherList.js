32dedf8a63875376f592755a340853a4f92be620
Haildottech <haildottech@gmail.com>
2024-01-09 13:05:03 +0500

    update

--- Components/Layouts/AccountsComp/Voucher/VoucherList.js
+++ Components/Layouts/AccountsComp/Voucher/VoucherList.js
@@ -17,59 +17,21 @@ const VoucherList = ({ voucherData }) => {
   const [originalData, setOriginalData] = useState();
   const dispatch = useDispatch();
 
-  const amountDetails = {
-    component: (props) => <>
-      <span style={{ color: 'grey' }}>Rs. </span>
-      <span className='blue-txt fw-6'>{commas(props.data.amount)}</span>
-    </>
+  const handleDelete = (id) => {
+    PopConfirm("Confirmation", "Are You Sure To Remove This Charge?",
+      () => {
+        axios.post(process.env.NEXT_PUBLIC_CLIMAX_POST_DELETE_BASE_VOUCHER, {
+          id: id
+        }).then((x) => {
+          Router.push("/accounts/voucherList")
+        })
+      })
+  }
+  const handleEdit = async (voucherId) => {
+    await Router.push(`/accounts/vouchers/${voucherId}`);
+    dispatch(incrementTab({ "label": "Voucher", "key": "3-5", "id": `${voucherId}` }));
   };
 
-  const genderDetails = {
-    component: (props) => <>
-      <span className='blue-txt fw-6 fs-12'>{props.data.voucher_Id}</span>
-    </>
-  };
-
-  const dateComp = {
-    component: (props) => <>
-      <span className='fw-6 fs-12'>{moment(props.data.createdAt).format("YYYY-MM-DD")}</span>
-    </>
-  };
-  const DeleteComp = {
-    component: (props) => <>
-      <div className='px-2'
-        onClick={() => {
-          PopConfirm("Confirmation", "Are You Sure To Remove This Charge?",
-            () => {
-              axios.post(process.env.NEXT_PUBLIC_CLIMAX_POST_DELETE_BASE_VOUCHER, {
-                id: props.data.id
-              }).then((x) => {
-                Router.push("/accounts/voucherList")
-              })
-            })
-        }}
-      >
-        <span className='fs-15 btn-red-two'><RiDeleteBin2Fill /></span>
-      </div>
-    </>
-  };
-
-  const [columnDefs, setColumnDefs] = useState([
-    // { headerName: '#', field: 'no', width: 40 },
-    { headerName: 'Voucher No.', field: 'voucher_Id', filter: true, filter: 'agTextColumnFilter', cellRendererSelector: () => genderDetails, filter: true },
-    { headerName: 'Type', field: 'type', filter: true , filter: 'agTextColumnFilter', },
-    { headerName: 'Cheque Date', field: 'date', filter: true, filter: 'agTextColumnFilter', },
-    { headerName: 'Paid To', field: 'payTo', filter: true , filter: 'agTextColumnFilter',},
-    { headerName: 'Amount', field: 'amount', filter: true, filter: 'agTextColumnFilter', cellRendererSelector: () => amountDetails, filter: true },
-    { headerName: 'Voucher Date', field: 'createdAt', filter: true, filter: 'agTextColumnFilter', cellRendererSelector: () => dateComp, filter: true },
-    { headerName: 'Delete', cellRendererSelector: () => DeleteComp },
-  ]);
-
-  const [offset, setOffset] = useState(0);
-  const [page, setPage] = useState(1);
-  const [count, setCount] = useState(0);
-  const [pageLoad, setPageLoad] = useState(false)
-  const [noMoreData, setNoMoreData] = useState(false);
   //search 
   const [query, setQuery] = useState("")
   const keys = ["voucher_Id", "type", "amount"]
@@ -170,11 +132,8 @@ const VoucherList = ({ voucherData }) => {
           </tbody>
         </Table>
       </div>
-      <div className='my-1'>
-        <button className='btn-custom-small px-3' onClick={() => offset != 0 ? nextPage(offset - 30) : null}>Previous</button>
-        <span className='mx-3 fs-20'>{page}</span>
-        <button className='btn-custom-small px-4' disabled={noMoreData} onClick={() => nextPage(offset + 30)}>Next</button>
-        {pageLoad && <Spinner size='sm' className='mx-4' color={'red'} />}
+      <div className='d-flex justify-content-end items-end my-4' style={{ maxWidth: "100%" }} >
+        <Pagination noOfPages={noOfPages} currentPage={currentPage} setCurrentPage={setCurrentPage} />
       </div>
     </div>
   );
