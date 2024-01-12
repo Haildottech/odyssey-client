18d44eae963aea1e1c7663b9bca720284c27a831
dev-status-200 <dev.status.200@gmail.com>
2024-01-12 16:41:45 +0500

    update

--- Components/Layouts/Setup/Employees/EmployeeAccess.js
+++ Components/Layouts/Setup/Employees/EmployeeAccess.js
@@ -1,8 +1,4 @@
-import { Form, Input, DatePicker, Select  } from "formik-antd";
-import { Formik, useField, useFormikContext } from 'formik';
 import React, { useState, useEffect, useRef } from 'react';
-import { Row, Col, Spinner } from 'react-bootstrap';
-import * as Yup from 'yup';
 import axios from 'axios';
 
 const EmployeeAccess = () => {
@@ -12,8 +8,6 @@ const EmployeeAccess = () => {
   })
 
   useEffect(() => {
-    // NEXT_PUBLIC_CLIMAX_POST_CREATE_EMPLOYEE_ACCESS ;
-    // NEXT_PUBLIC_CLIMAX_GET_ALL_EMPLOYEES ;
     fetchData();
     console.log('Acess Page') ;
   }, [])
